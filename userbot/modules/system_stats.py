af78682389f8025d4225e6d048a2a176d3f4d713
abubarst12 <59693447+abubarst12@users.noreply.github.com>
2020-01-10 20:05:28 +0530

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -119,11 +119,11 @@ async def pipcheck(pip):
         await pip.edit("`Use .help pip to see an example`")
 
 
-@register(outgoing=True, pattern="^.on$")
-async def amireallyalive(on):
-    """ For .on command, check if the bot is running.  """
+@register(outgoing=True, pattern="^.alive$")
+async def amireallyalive(alive):
+    """ For .alive command, check if the bot is running.  """
     await on.edit("`"
-                     "i am ON My Mastor \n\n"
+                     "i am Alive My Mastor \n\n"
                      " \n\n"
                      "botdo is ON \n\n"
                      f"Telethon version: {version.__version__} \n"
@@ -170,9 +170,9 @@ CMD_HELP.update(
     {"pip": ".pip <module(s)>\
     \nUsage: Does a search of pip modules(s)."})
 CMD_HELP.update({
-    "on":
-    ".on\
-    \nUsage: Type .on to see wether your bot is working or not.\
+    "alive":
+    ".alive\
+    \nUsage: Type .alive to see wether your bot is working or not.\
     \n\n.aliveu <text>\
     \nUsage: Changes the 'user' in alive to the text you want.\
     \n\n.resetalive\
